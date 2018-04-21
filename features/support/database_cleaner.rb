d2be3231c5f2119fb94057ae15edb88d6d44711b
Micah Geisel <micah@botandrose.com>
2018-04-21 16:41:39 -0700

    fix tests.

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,6 +1,7 @@
 require 'database_cleaner'
 require 'database_cleaner/cucumber'
 
+DatabaseCleaner.allow_remote_database_url = true # sqlite:// counts as remote. PR?
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean_with :truncation
 
